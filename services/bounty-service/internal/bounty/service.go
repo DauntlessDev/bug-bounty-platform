e8655a138a7c55e36566f76ea9610cedabadc882
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2025-06-06 16:36:53 +0800

    feat(bounty-service): Add initial service

--- services/bounty-service/internal/bounty/service.go
+++ services/bounty-service/internal/bounty/service.go
@@ -6,3 +6,27 @@ type Repository interface {
 	CreateBounty(bounty *Bounty) error
 	UpdateBounty(bounty *Bounty) error
 }
+
+type Service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *Service {
+	return &Service{repository: repository}
+}
+
+func (s *Service) GetBounties() ([]Bounty, error) {
+	return s.repository.GetBounties()
+}
+
+func (s *Service) GetBountiesBy(id string) (Bounty, error) {
+	return s.repository.GetBountyByID(id)
+}
+
+func (s *Service) CreateBounty(bounty *Bounty) error {
+	return s.repository.CreateBounty(bounty)
+}
+
+func (s *Service) UpdateBounty(bounty *Bounty) error {
+	return s.repository.UpdateBounty(bounty)
+}
